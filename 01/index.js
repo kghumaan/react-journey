793a6aba912ad69be37e28be15f61fa4d617a459
KV Ghumaan <kv.s.ghumaan@gmail.com>
2021-11-20 15:16:38 -0500

    rendering a list

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'))
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+ReactDOM.render(
+    <ul>
+        <li>first item</li>
+        <li>second item</li>
+    </ul>, document.getElementById('root'))
