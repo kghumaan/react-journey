a1a84318e024a6f5782a968ed386005c3eebe613
KV Ghumaan <kv.s.ghumaan@gmail.com>
2021-11-25 15:11:24 -0500

    webpack setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
+
+
+export function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+}
